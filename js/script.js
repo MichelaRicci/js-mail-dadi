df4c34abed58fc4e42b101dd0ab6fb7ce3a77cf5
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-12 16:40:40 +0100

    stampa in pagina risultati

--- js/script.js
+++ js/script.js
@@ -4,25 +4,46 @@ console.log('JS OK');
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Stampiamo in pagina i due tiri e il risultato
 
+
 // Inserire il pulsante
 
 const number = document.getElementById('btn');
 
+const result = document.getElementById('result');
 
-//collego pulsante a event listener
+let content = '';
+
+
+// Collego pulsante a event listener
 
 number.addEventListener("click", function(){
     let player = Math.floor(Math.random() * 6 + 1);
     let computer = Math.floor(Math.random() * 6 + 1);
 
+
     console.log(player, computer);
+
 
     if (player > computer) {
         console.log("Hai vinto!");
+
+        content = "Hai vinto!" + `<p>${player} + ${computer}</p>`;
+
+
     } else if (computer > player) {
         console.log("Hai perso!");
+
+        content = "Hai perso!" + `<p>${player} + ${computer}</p>`;
     } else {
         console.log("Pareggio!");
+
+        content = "Pareggio" + `<p>${player} + ${computer}</p>`;
     }
+
+
+    result.innerHTML = content;
+
 })
+
+
 
