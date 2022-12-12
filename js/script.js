7acf3c8132c1823c5ff9bcd64cafab8baad72282
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-12 17:32:11 +0100

    btn mail collegato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 console.log('JS OK');
 
+
+// # DADI ---------------------------------------------------------------
+
+
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Stampiamo in pagina i due tiri e il risultato
 
 
-// Inserire il pulsante
+// Inserire il pulsante e gli elementi
 
-const number = document.getElementById('btn');
+const number = document.getElementById('btnDice');
 
 const result = document.getElementById('result');
 
@@ -27,23 +31,48 @@ number.addEventListener("click", function(){
     if (player > computer) {
         console.log("Hai vinto!");
 
-        content = "Hai vinto!" + `<p>${player} + ${computer}</p>`;
+        content =`<p>${player} + ${computer}</p>` + "Hai vinto!";
 
 
     } else if (computer > player) {
         console.log("Hai perso!");
 
-        content = "Hai perso!" + `<p>${player} + ${computer}</p>`;
+        content =`<p>${player} + ${computer}</p>` + "Hai perso!";
     } else {
         console.log("Pareggio!");
 
-        content = "Pareggio" + `<p>${player} + ${computer}</p>`;
+        content =`<p>${player} + ${computer}</p>` + "Pareggio!";
     }
 
-
+ // Stampo in pagina
     result.innerHTML = content;
 
 })
+
+
+
+// EMAIL -------------------------------------------------------------------
+
+// Inventa una lista di email autorizzate
+// Chiedi all’utente la sua email, con un piccolo form.
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Inserire il pulsante e gli elementi
+
+const btnMail = document.getElementById('btnMail');
+
+const validMail = ["tizio@mail.com", "caio@mail.com", "sempronio@mail.com"];
+
+
+
+
+
+
+
+
+
 
 
 
